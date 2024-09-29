bbb172192b74cde437a58da47b1708776933edf8
Alireza Aslani <c.computerdev@yahoo.com>
2024-09-29 19:13:35 +0330

    upload http endpoint

--- main.go
+++ main.go
@@ -4,18 +4,41 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 )
 
 func main() {
 
 	r := gin.Default()
 
-	r.GET("/hello/:name", func(c *gin.Context) {
+	r.POST("/upload", func(c *gin.Context) {
 
-		name := c.Param("name")
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		var fileSize int64 = 5
+
+		if file.Size > fileSize<<20 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": fmt.Errorf("file size exceeds maximum limit %d MB", fileSize).Error(),
+			})
+			return
+		}
+
+		if err = c.SaveUploadedFile(file, fmt.Sprintf("./tmp/%s", file.Filename)); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 
 		c.JSON(200, gin.H{
-			"message": fmt.Sprintf("Hello %s", name),
+			"message": fmt.Sprintf("File has been uploaded, %s", file.Filename),
 		})
 	})
 
